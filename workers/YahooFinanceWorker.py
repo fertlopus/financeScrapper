4446123cb02ffa0ff731abe407dfa9c6c1487494
fertlopus <marat.movlamov@icloud.com>
2024-01-26 12:15:28 +0100

    Yahoo Finance Worker modified. Queue added

--- workers/YahooFinanceWorker.py
+++ workers/YahooFinanceWorker.py
@@ -1,26 +1,41 @@
+import queue
 import threading
 import requests
 from lxml import html
+import datetime
 import time
 import random
 
 
 class YahooFinanceScheduler(threading.Thread):
-    def __init__(self, in_queue, **kwargs):
+    def __init__(self, input_queue, output_queue, **kwargs):
         super(YahooFinanceScheduler, self).__init__(**kwargs)
         # take the input thread and start the execution
-        self._in_queue = in_queue
+        self._in_queue = input_queue
+        temp_queue = output_queue
+        if not isinstance(temp_queue, list):
+            temp_queue = [temp_queue]
+        self._output_queue = temp_queue
         self.start()
 
     def run(self):
         while True:
             # Blocking operation until value is returned
-            value = self._in_queue.get()
+            try:
+                value = self._in_queue.get(timeout = 10)
+            except queue.Empty:
+                print("Yahoo Scheduler has exited due to queue empty")
+                break
             if value == 'DONE':
+                for output_queue in self._output_queue:
+                    output_queue.put('DONE')
                 break
 
             yahoo_finance_worker = YahooFinanceWorker(ticker = value)
             price = yahoo_finance_worker.get_ticker_price()
+            for output_queue in self._output_queue:
+                output_values = (value, price, datetime.datetime.utcnow())
+                output_queue.put(output_values)
             time.sleep(random.random())
 
 
